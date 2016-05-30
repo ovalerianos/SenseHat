eb1ec08edd2d5c7e9761028d646534556bc59b02
Omar Valeriano <ovalerianos@gmail.com>
2016-05-30 18:37:55 -0500

    Update Gusano.py

--- Gusano.py
+++ Gusano.py
@@ -1,11 +1,28 @@
 
 
 
-def actualizaGusano(gusano,bloque):
+
+
+def updateGusano(gusano,bloque):
 	count  = len(gusano)-1
-	lstNuevoGusano = newArray
+	lstNuevoGusano = range(len(gusano))
 	#Recorre el gusano antiguo y lo pone en el nuevo pero descartando
 	#la ultima posicion
 	while count > 0 :
 		lstNuevoGusano[count] = gusano[count-1]
+		count = count - 1
 	lstNuevoGusano[0] = bloque
+
+	return lstNuevoGusano
+
+def addGusano(gusano,bloque):
+	count  = len(gusano)-1
+	lstNuevoGusanoNew = range(len(gusano)+1)
+	#Recorre el gusano antiguo y lo pone en el nuevo pero descartando
+	#la ultima posicion
+	while count > 0 :
+		lstNuevoGusanoNew[count] = gusano[count-1]
+		count = count - 1
+	lstNuevoGusanoNew[0] = bloque
+
+	return lstNuevoGusanoNew
